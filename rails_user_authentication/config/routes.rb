aa0e974c0d0e7756bd4b6d060208b75d9cbd1639
gkemmey <gray.kemmey@gmail.com>
2016-05-26 12:35:09 -0400

    removes existing routes

--- rails_user_authentication/config/routes.rb
+++ rails_user_authentication/config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
